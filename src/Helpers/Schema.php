ccbc2d8c76c2b050600024bb406a2da0d09e8ac1
Fujiwara Satoshi <f.satoshi@famm.tech>
2025-01-16 00:58:13 +0900

    update

---
+++ src/Helpers/Schema.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace FammSupport\Helpers;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+
+class Schema
+{
+    const TYPE_ID = 'Id';
+
+    const TYPE_STRING = 'String';
+
+    const TYPE_ASSOCIATION = 'Association';
+
+    const TYPE_LOOKUP = 'Lookup';
+
+    const TYPE_TIMESTAMP = 'Timestamp';
+
+    const TYPE_BIG_INT = 'BigInt';
+
+    const TYPE_JAPAN_PERSON_NAME = 'JapanPersonName';
+
+    const TYPE_SELECT = 'Select';
+
+    const TYPE_EMAIL = 'Email';
+
+    const TYPE_PASSWORD = 'Password';
+
+    const TYPE_INT = 'Int';
+
+    const TYPE_TEXT = 'Text';
+
+    const TYPE_DATE = 'Date';
+
+    const TYPE_JAPAN_ADDRESS = 'JapanAddress';
+
+    const TYPE_ADDRESS = 'Address';
+
+    const RELATION_MANY_TO_ONE = 'ManyToOne';
+
+    const RELATION_MANY_TO_MANY = 'ManyToMany';
+
+    const RELATION_ONE_TO_MANY = 'OneToMany';
+
+    private Collection $schemas;
+
+    public function __construct()
+    {
+        $schemas = collect();
+        if (File::exists(famm_path('schema-lock.json'))) {
+            $schemas = collect(json_decode(File::get(famm_path('schema-lock.json')), 1) ?? []);
+        }
+        $this->schemas = $schemas;
+    }
+
+    public function all(): Collection
+    {
+        return $this->schemas;
+    }
+
+    public function get($entityName): ?Collection
+    {
+        return collect($this->schemas[$entityName]) ?? null;
+    }
+
+    public function getTarget($entityName, $attributeName): ?Collection
+    {
+        $entity = $this->get($entityName);
+
+        return collect($this->get($entity['attributes'][$attributeName]['target'])) ?? null;
+    }
+}
---
+++ src/Helpers/View.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FammSupport\Helpers;
+
+use FammSupport\DynamicSchemaProcessor;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+
+class View
+{
+    private Collection $schemas;
+
+    public function __construct()
+    {
+        $schemas = collect();
+        if (File::exists(famm_path('view-lock.json'))) {
+            $schemas = collect(json_decode(File::get(famm_path('view-lock.json')), 1) ?? []);
+        }
+        $this->schemas = $schemas;
+    }
+
+    public function all(): Collection
+    {
+        return $this->schemas;
+    }
+
+    public function get($entityName, $viewType = 'list'): ?Collection
+    {
+        $processor = new DynamicSchemaProcessor;
+
+        return collect($processor->processSchema($this->schemas[$entityName . '::' . $viewType])) ?? null;
+    }
+}
