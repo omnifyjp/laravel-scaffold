d4cd41d46b0343301e5318fd6801eb376e881d45
Fujiwara Satoshi <26842626+ecsol@users.noreply.github.com>
2025-04-17 18:08:34 +0900

    update

--- routes/support.php
+++ routes/support.php
@@ -1,20 +1,13 @@
 <?php
 
-use OmnifyJP\LaravelScaffold\Http\Controllers\Api\CollectionController;
-use OmnifyJP\LaravelScaffold\Http\Controllers\Api\ObjectController;
-use OmnifyJP\LaravelScaffold\Http\Controllers\Api\SelectController;
 use Illuminate\Support\Facades\Route;
+use OmnifyJP\LaravelScaffold\Http\Controllers\Api\CollectionController;
+use OmnifyJP\LaravelScaffold\Http\Controllers\Api\SelectController;
 
 Route::middleware(['web'])->group(function () {
     Route::prefix('api/v1')->group(function () {
         Route::get('selects', [SelectController::class, 'list'])->name('api.selects');
         Route::get('selects/{selectName}', [SelectController::class, 'show'])->name('api.selects.show');
-        Route::get('objects', [ObjectController::class, 'list'])->name('api.objects');
-        Route::get('objects/{objectName}', [ObjectController::class, 'getObject'])->name('api.objects.get-object');
-        Route::get('objects/{objectName}/{propertyName}', [ObjectController::class, 'getProperty'])->name('api.objects.get-property');
-        Route::post('objects/{objectName}/{propertyName}/upload', [ObjectController::class, 'upload'])->name('api.objects.upload');
-
         Route::get('data/{objectName}', [CollectionController::class, 'index'])->name('api.objects.index');
-
     });
 });
